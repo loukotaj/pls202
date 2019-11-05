898cea2adedc4c752d7dd6ad1c47e30f872f4d5f
loukotaj <jacobloukota@gmail.com>
2019-11-04 20:31:44 -0500

    Update teamProject2.r

--- teamProject2.r
+++ teamProject2.r
@@ -8,14 +8,16 @@ destfile <- "GCP_Release_1.xlsx"
 curl::curl_download(url, destfile)
 GCP_Release_1 <- read_excel(destfile)
 
-
+#data ingestion and cleaning
 names(GCP_Release_1)[6]<-"gdp"
 GCP_Release_1$gdp_num <- as.numeric(GCP_Release_1$gdp)
 GCP_Release_1$subregion <- tolower(GCP_Release_1$Countyname)
 gdp_data =  data.frame(GCP_Release_1$gdp_num,GCP_Release_1$subregion,GCP_Release_1$LineCode)
+#we just care about total gdp drop the industry info
 gdp_data = gdp_data[gdp_data$GCP_Release_1.LineCode == 1,]
 names(gdp_data)[1] <- "gdp"
 names(gdp_data)[2] <- "subregion"
+#make_base_map
 usa <- map_data("usa")
 us_base <- ggplot(data=usa, mapping = aes(x=long, y=lat, group=group))+
   coord_fixed(1.3) +
@@ -23,12 +25,13 @@ us_base <- ggplot(data=usa, mapping = aes(x=long, y=lat, group=group))+
 # merge gdp data
 counties <- map_data("county")
 usgdp <- inner_join(counties, gdp_data, by="subregion")
+#make final map
 density <- us_base +
   geom_polygon(data=usgdp,aes(fill=gdp),color="white")+
   geom_polygon(color="black", fill=NA) +
   theme_bw() +
   scale_fill_gradient(trans="log10")
-
+#pretty colors
 density2 <- density +
   scale_fill_gradientn(
     colors = rev(rainbow(7)),
